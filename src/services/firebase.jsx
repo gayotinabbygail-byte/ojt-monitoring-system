99ece16e6e484dc51c9c5dfcbc4736a0c966f640
TNG <kennsantos211@gmail.com>
2026-08-01 13:05:46 +0800

    Create firebase.jsx

---
+++ src/services/firebase.jsx
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC3hojm6uK_0ldhjhdpPOmCcvLfyZh98xg",
+  authDomain: "ojtmonitoringsystem-6c92f.firebaseapp.com",
+  projectId: "ojtmonitoringsystem-6c92f",
+  storageBucket: "ojtmonitoringsystem-6c92f.firebasestorage.app",
+  messagingSenderId: "855244812028",
+  appId: "1:855244812028:web:3cc71b543bd037fb8eb179",
+  measurementId: "G-KG67XG1PXE"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
