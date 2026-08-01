472cbc4832da87081e7084f9ad42f10c25547cbf
TNG <kennsantos211@gmail.com>
2026-08-01 15:26:52 +0800

    update

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -3,53 +3,64 @@ import { login } from "../services/authService";
 import { useState } from "react";
 
 function Login() {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const handleLogin = (e) => {
-        e.preventDefault();
+  const handleLogin = async (e) => {
+    e.preventDefault();
 
-        if (!email || !password) {
-            alert("Please enter both email and password.");
-            return;
-        }
-        console.log("Email;", email);
-        console.log("Password:", password);
+    if (!email || !password) {
+      alert("Please enter both email and password.");
+      return;
     }
-    return (
-        <div className="login-container">
-            <div className="login-card">
-                <h1>Voting System</h1>
-                <p>Please sign in to continue.</p>
+    console.log("Email;", email);
+    console.log("Password:", password);
+  };
+  return;
 
-                <form onSubmit={handleLogin}>
-                    <div className="input-group">
-                        <label>Email</label>
-                        <input
-                            type="email"
-                            placeholder="Enter your email"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                        />
-                    </div>
+    try {
+        const userCredential = await Login(email, password);
 
-                    <div className="input-group">
-                        <label>Password</label>
-                        <input
-                            type="password"
-                            placeholder="Enter your password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                        />
-                    </div>
+        console.log("Login Successful"),
+        console.log(userCredential.user);
 
-                    <button type="submit">
-                        Login
-                    </button>
-                </form>
-            </div>
-        </div>
-    );
-}
+        alert("Login Successful");
+    } catch (error) {
+
+        alert(error.message);
+    }
+    }
+
+    <div className="login-container">
+      <div className="login-card">
+        <h1>Voting System</h1>
+        <p>Please sign in to continue.</p>
+
+        <form onSubmit={handleLogin}>
+          <div className="input-group">
+            <label>Email</label>
+            <input
+              type="email"
+              placeholder="Enter your email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+
+          <div className="input-group">
+            <label>Password</label>
+            <input
+              type="password"
+              placeholder="Enter your password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    </div>
+
 
 export default Login;
