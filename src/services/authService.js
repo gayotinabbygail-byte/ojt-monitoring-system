e581d880a5c2725441162920cdc7fb6c2ae55c26
TNG <kennsantos211@gmail.com>
2026-08-08 09:27:08 +0800

    update

---
+++ src/services/authService.js
@@ -0,0 +1,6 @@
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "./firebase";
+
+export const login = async (email, password) => {
+return await signInWithEmailAndPassword(auth, email, password);
+};
